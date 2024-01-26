e8f2438b588aff620b0ae182d94b6643636fc497
aayush.kushwaha107 <1728107@kiit.ac.in>
2024-01-26 12:56:17 +0530

    assignments

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Calculator");
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        float first_num = sc.nextFloat();
+        System.out.println("Enter second number:");
+        float second_num = sc.nextFloat();
+        float sum = first_num + second_num;
+        float diff = first_num - second_num;
+        float mul = first_num * second_num;
+        float div = first_num / second_num;
+        float modulo = first_num % second_num;
+        System.out.println("The sum is " + sum + " and the difference is " + diff + " and the product is " + mul + " and the division is " + div + " and the modulo is " + modulo);
+    }
+}
